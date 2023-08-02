ab5ed3adb7c6ea58562bc38089a5dca31d9d6e0f
dorlov <74orlov@gmail.com>
2023-08-02 19:20:06 +0100

    Updates

---
+++ precache-manifest.b06b887dc15bad5946d99907de3cf42b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "05a99a7e32cb810438be",
+    "url": "/landing/static/css/main.49c6e6fa.chunk.css"
+  },
+  {
+    "revision": "05a99a7e32cb810438be",
+    "url": "/landing/static/js/main.05a99a7e.chunk.js"
+  },
+  {
+    "revision": "03628066470ce55ea2f4",
+    "url": "/landing/static/js/1.03628066.chunk.js"
+  },
+  {
+    "revision": "2cb16e025927c8336b04",
+    "url": "/landing/static/css/2.084473cb.chunk.css"
+  },
+  {
+    "revision": "2cb16e025927c8336b04",
+    "url": "/landing/static/js/2.2cb16e02.chunk.js"
+  },
+  {
+    "revision": "d43767c75c9b01a60394",
+    "url": "/landing/static/js/runtime~main.d43767c7.js"
+  },
+  {
+    "revision": "e9de9e808ccbc962277a7674de6044e6",
+    "url": "/landing/static/media/react_icon.e9de9e80.svg"
+  },
+  {
+    "revision": "e14021aa73c66564d692da79d3fd3f27",
+    "url": "/landing/static/media/typescript.e14021aa.svg"
+  },
+  {
+    "revision": "c96880b71db7336de07f63132c80d7a4",
+    "url": "/landing/static/media/materialui.c96880b7.svg"
+  },
+  {
+    "revision": "32a8566c1ba8900b27efe61ff06ec791",
+    "url": "/landing/static/media/firebase_.32a8566c.svg"
+  },
+  {
+    "revision": "a7832c822b6bdfbf925e4f9fbfba0028",
+    "url": "/landing/static/media/Project1.a7832c82.png"
+  },
+  {
+    "revision": "579ba4a99bcea901639ffb0c9b1d67cb",
+    "url": "/landing/static/media/telegram.579ba4a9.svg"
+  },
+  {
+    "revision": "b716e2c72a9c8399539c200a3abb0996",
+    "url": "/landing/static/media/dot_net_icon.b716e2c7.svg"
+  },
+  {
+    "revision": "25c71b71ea530205da602da0f5de18a4",
+    "url": "/landing/static/media/api_icon.25c71b71.svg"
+  },
+  {
+    "revision": "f63ad17051522df3ea17344f111c9064",
+    "url": "/landing/static/media/javascript.f63ad170.svg"
+  },
+  {
+    "revision": "1078a0936febd7fdd300b4652ec35b3f",
+    "url": "/landing/static/media/html.1078a093.svg"
+  },
+  {
+    "revision": "3f66d4cbdc8e3bfaf0e3270b19567d63",
+    "url": "/landing/static/media/css3.3f66d4cb.svg"
+  },
+  {
+    "revision": "b09f4e6b173f76f29e401c9d005aea08",
+    "url": "/landing/static/media/c++.b09f4e6b.svg"
+  },
+  {
+    "revision": "4d7ca4bce7763c8f3e5067058355691c",
+    "url": "/landing/static/media/c-sharp.4d7ca4bc.svg"
+  },
+  {
+    "revision": "14e769fab1357afd99ba7d8b75c46cc0",
+    "url": "/landing/static/media/azure.14e769fa.svg"
+  },
+  {
+    "revision": "ed3f52f02dab382c1b8b87044995a786",
+    "url": "/landing/static/media/visual-studio_.ed3f52f0.svg"
+  },
+  {
+    "revision": "9c8c66608526d7ab00eeae87d11be8ac",
+    "url": "/landing/static/media/visual-studio-code.9c8c6660.svg"
+  },
+  {
+    "revision": "116bb462096fd46f79cd368994be5e19",
+    "url": "/landing/static/media/docker.116bb462.svg"
+  },
+  {
+    "revision": "bfaa45fb7a04d96438594e359ceb1ce8",
+    "url": "/landing/static/media/git.bfaa45fb.svg"
+  },
+  {
+    "revision": "e2e465faf16594f45ebfa7f087c62d90",
+    "url": "/landing/static/media/figma.e2e465fa.svg"
+  },
+  {
+    "revision": "528130cc45ae6c1507fb4f5ede62de7a",
+    "url": "/landing/static/media/photoshop.528130cc.svg"
+  },
+  {
+    "revision": "e117ea72f4478c9ad9c6861e98b89fac",
+    "url": "/landing/static/media/illustrator.e117ea72.svg"
+  },
+  {
+    "revision": "61c2a4ea3b2713f15f984e62ac72012e",
+    "url": "/landing/static/media/msql-server.61c2a4ea.svg"
+  },
+  {
+    "revision": "ac480dbbbc0cc2cde9df6c561f711370",
+    "url": "/landing/static/media/mongodb.ac480dbb.svg"
+  },
+  {
+    "revision": "41e9605316293a1b1c13877ead3bbaef",
+    "url": "/landing/static/media/postgres.41e96053.svg"
+  },
+  {
+    "revision": "69e319a8cd9fd2a2fde4ad91afe2d116",
+    "url": "/landing/static/media/mysql_.69e319a8.svg"
+  },
+  {
+    "revision": "351044fdb6003c1f17f6480d75c77a38",
+    "url": "/landing/static/media/autodesk.351044fd.svg"
+  },
+  {
+    "revision": "54454441e1e54dd82d94711f33f156a6",
+    "url": "/landing/static/media/linkedin.54454441.svg"
+  },
+  {
+    "revision": "cc59775a76786a8045682d60a218dab4",
+    "url": "/landing/static/media/league-spartan-vietnamese-wght-normal.cc59775a.woff2"
+  },
+  {
+    "revision": "9b94fa5976c09e792420aa62a897cdda",
+    "url": "/landing/static/media/league-spartan-latin-ext-wght-normal.9b94fa59.woff2"
+  },
+  {
+    "revision": "562993773f212d0b11fbcbe2aee33aba",
+    "url": "/landing/static/media/league-spartan-latin-wght-normal.56299377.woff2"
+  },
+  {
+    "revision": "853b2d73241fb8ad2ee5ee2ad0279e60",
+    "url": "/landing/index.html"
+  }
+];
